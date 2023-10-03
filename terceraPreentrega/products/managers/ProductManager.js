6159633d5880ebdbef18fcb7e232e9b362e8a8c9
Nicolás Caminos <nicolas_caminos@outlook.com>
2023-10-03 18:43:07 -0300

    Preentrega3: Nicolas_caminos

---
+++ terceraPreentrega/products/managers/ProductManager.js
@@ -0,0 +1,90 @@
+import fs from 'fs';
+
+export default class ProductManager {
+    constructor(path) {
+        this.path = path;
+    }
+
+    getProducts = async () => {
+        if (fs.existsSync(this.path)) {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(data);
+            return products;
+        }
+        return [];
+    }
+
+    addProducts = async (product) => {
+        try {
+            const products = await this.getProducts();
+            if (products.find(p => p.code === product.code)) {
+                throw new Error("El producto ya existe"); // Lanza un error si el producto ya existe
+            }
+            if (products.length === 0) {
+                product.id = 1;
+            } else {
+                product.id = products[products.length - 1].id + 1;
+            }
+            products.push(product);
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+            return product; // Retorna el producto recién agregado
+        } catch (error) {
+            console.error("Error al agregar el producto:", error);
+            throw error;
+        }
+    }
+    getProductById = async (searchById) => {
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(data);
+            const product = products.find(p => p.id === searchById);
+
+            if (product) {
+                return product;
+            } else {
+                throw new Error("El producto no existe");
+            }
+        } catch (error) {
+            // Manejo de errores, puedes imprimir el mensaje de error o lanzar la excepción nuevamente si es necesario
+            console.error("Error al obtener el producto:", error);
+            throw error; // Opcionalmente, puedes lanzar la excepción nuevamente para que quien llame a esta función maneje el error
+        }
+    }
+
+    deleteProduct = async (_id) => {
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(data);
+            const index = products.findIndex(product => product.id === _id);
+
+            if (index === -1) {
+                throw new Error("El producto no existe");
+            }
+
+            products.splice(index, 1);
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+        } catch (error) {
+            console.error("Error al eliminar el producto:", error);
+            throw error;
+        }
+    }
+
+    updateProduct = async (_id, attribute, value) => {
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(data);
+            const product = products.find(p => p.code === _id);
+
+            if (!product) {
+                throw new Error("El producto no existe");
+            }
+
+            product[attribute] = value;
+
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+        } catch (error) {
+            console.error("Error al actualizar el producto:", error);
+            throw error;
+        }
+    }
+}
