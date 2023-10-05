61f90afc2579592ef28fe800291e766ecd0fd23c
Nicolás Caminos <nicolas_caminos@outlook.com>
2023-10-05 18:17:51 -0300

    update_indents

--- desafio1/primeraPreentrega.js
+++ desafio1/primeraPreentrega.js
@@ -3,16 +3,14 @@ class ProductManager {
     constructor() {
         this.products = [];
     };
-    // metodos
+
     addProducts = ({ title, description, price, thumbnail, code, stock }) => {
 
-        // valido que vengan todos los datos por parametro
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             console.info("Debe incluir todos los parametros.")
             return;
         }
 
-        // valido que el code no exista previamente
         const foundCode = this.products.find(e => e.code === code);
         if (foundCode) {
             console.info("El 'code' ya existe previamente")
@@ -35,7 +33,6 @@ class ProductManager {
         } {
             product.id = id + 1
         }
-
         this.products.push(product)
     };
 
@@ -59,11 +56,7 @@ class ProductManager {
         console.log(this.products[searchId - 1])
     }
 };
-
-// creo mi instancia del product 1
 const product1 = new ProductManager()
-
-// creo un objeto de testeo para los metodos
 const testProduct = {
     title: "producto prueba",
     description: "Este es un producto prueba",
@@ -73,12 +66,11 @@ const testProduct = {
     stock: 25
 }
 
-// invoco metodos para controlar el resultado
-product1.getProducts() // muestra el array vacio creado en el constructor
-product1.addProducts(testProduct); // agrega correctamente el producto al array
-product1.getProducts() // muestra el array con el primer producto agregado
-product1.addProducts(testProduct); // devuelve error porque ya existe el code cargado previamente
-product1.getProductById();  // devuelve error al no enviar el parametro de busqueda
-product1.getProductById(1); // si encuentra el 1, por ende muestra el objeto
-product1.getProductById(2); // no encuentra el 1, por ende no hace nada
+product1.getProducts()
+product1.addProducts(testProduct);
+product1.getProducts()
+product1.addProducts(testProduct);
+product1.getProductById();
+product1.getProductById(1);
+product1.getProductById(2);
 
--- desafio2/index.js
+++ desafio2/index.js
@@ -2,13 +2,11 @@ import ProductManager from './products/managers/ProductManager.js';
 
 // Ruta del archivo JSON
 const filePath = './products/files/productos.json';
-
 const productManager = new ProductManager(filePath);
-
-// Definir una función para ejecutar el contexto
 const context = async () => {
     const test = await productManager.getProducts();
     console.log(test);
+
     let productTest = {
         title: 'producto prueba 6',
         description: 'Este es un producto prueba 6',
@@ -19,12 +17,9 @@ const context = async () => {
     }
 
      await productManager.addProducts(productTest);
-
      const newUsers = await productManager.getProducts();
      console.log(newUsers);
-
      const busquedaPorId = await productManager.getProductById(1);
-
      console.log(busquedaPorId)
      await productManager.updateProduct('abc123', 'description', 'Producto de prueba editado 2')
      console.log(await productManager.getProductById(1));
