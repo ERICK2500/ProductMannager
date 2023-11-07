4ca3de0f3bcbf07d1a4ba36b61d120bc0d8d3eda
Nicolás Caminos <nicolas_caminos@outlook.com>
2023-11-07 20:48:47 -0300

    update

---
+++ desafio5/src/util.socket.js
@@ -0,0 +1,45 @@
+import ProductManager from './dao/mongo/managers/ProductManager.js';
+import path from 'path';
+import __dirname from './utill.js';
+
+const filePath = path.resolve(
+    __dirname,
+    'dao',
+    'fileSystem',
+    'products',
+    'files',
+    'productos.json'
+);
+const pm = new ProductManager(filePath)
+
+export default function socketProducts(socketServer) {
+    socketServer.on('connection', async socket => {
+
+        const data = await pm.getProducts()
+
+        socket.emit('products', { data })
+
+        socket.on('product', async data => {
+
+            try {
+                const valueReturned = await pm.addProduct(data)
+
+                socket.emit('message', valueReturned)
+            }
+            catch (err) {
+                console.log(err);
+            }
+
+            
+        })
+
+        socket.on('delete', async data => {
+
+            const result = await pm.deleteProduct(data)
+
+            socket.emit('delete', result)
+        })
+
+
+    })
+}
