425450d0846a831675fb85c67f5a4e7cbb523da2
Nicolás Maximiliano Caminos <45422512+NicolasCaminos@users.noreply.github.com>
2024-01-26 03:39:22 -0300

    Actualizar mailer.js

--- desafio12/src/utils/mailer.js
+++ desafio12/src/utils/mailer.js
@@ -2,7 +2,8 @@ import nodemailer from 'nodemailer';
 import config from '../config/config.js';
 export const transport = nodemailer.createTransport({
     service: 'gmail',
-    port: 587,
+    port: 465,
+    secure: true,
     auth:{
         user:config.user_mail,
         pass:config.pass_mail
